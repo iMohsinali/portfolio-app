8aeb2c3910477be17a1593931016d36af00ba956
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-02-24 20:16:44 +0500

    Create AppWrapper.js

---
+++ frontend/src/wrapper/AppWrapper.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Nevegation from "../components/Nevegation";
+import SocialMedia from "../components/SocialMedia";
+
+const AppWrapper = () => {
+  return <div></div>;
+};
+
+export default AppWrapper;
