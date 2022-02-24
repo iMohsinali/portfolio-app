1ecd1478948c9ed1e526ca1a4d1ed14d25a1cfa6
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-02-24 16:25:12 +0500

    add sanity

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,52 +1,41 @@
-import React, { useEffect, useState } from "react";
-import "./About.scss";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
-const About = () => {
-  const object = [
-    {
-      titile: "Web Developer",
-      desc: "I am a Good Web developer",
-      imgurl: images.about01,
-    },
-    {
-      titile: "Web Design",
-      desc: "I am a Good Web Desiging",
-      imgurl: images.about02,
-    },
-    {
-      titile: "UI/UX",
-      desc: "I am a Good UI/UX Designer",
-      imgurl: images.about03,
-    },
+import "./About.scss";
+import { urlfor, client } from "../../clients";
 
-    {
-      titile: "Programmer",
-      desc: "I am a Good Programmer",
-      imgurl: images.about04,
-    },
-  ];
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I know That <span>Good Dev</span>
-        <br /> mean<span> Good Busnisses</span>
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
       </h2>
+
       <div className="app__profiles">
-        {object.map((e, i) => (
+        {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-items"
-            key={e.titile + i}
+            key={about.title + index}
           >
-            <img src={e.imgurl} alt={e.titile} srcset="" />
-            <h2 className="blod-text" style={{ marginTop: 20 }}>
-              {e.titile}
+            <img src={urlfor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
             </h2>
             <p className="p-text" style={{ marginTop: 10 }}>
-              {e.desc}
+              {about.description}
             </p>
           </motion.div>
         ))}
@@ -54,5 +43,4 @@ const About = () => {
     </>
   );
 };
-
 export default About;
