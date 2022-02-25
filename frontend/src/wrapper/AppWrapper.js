be77b7e3ab413a5807f368f3ec9b70f92799eba9
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-02-25 15:48:28 +0500

    Update AppWrapper.js

--- frontend/src/wrapper/AppWrapper.js
+++ frontend/src/wrapper/AppWrapper.js
@@ -1,9 +1,22 @@
 import React from "react";
-import Nevegation from "../components/Nevegation";
+import NavigationDots from "../components/NavigationDots";
 import SocialMedia from "../components/SocialMedia";
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className="app__wrapper app__flex">
+          <Component />
 
-const AppWrapper = () => {
-  return <div></div>;
-};
+          <div className="copyright">
+            <p className="p-text">@2020 Mohsin</p>
+            <p className="p-text">All rights reserved</p>
+          </div>
+        </div>
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
 
-export default AppWrapper;
+export default AppWrap;
